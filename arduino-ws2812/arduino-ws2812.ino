4916f74a39a84a10fb88c551ea143d1d8ef9be09
Chris Saiko <chris.saiko@gmail.com>
2021-03-26 16:31:09 -0500

    Create starting arduino code

---
+++ arduino-ws2812/arduino-ws2812.ino
@@ -0,0 +1,35 @@
+/*  File name:    arduino-ws2812.ino
+ *  Description:  Arduino code to control a number of LED strips using individual ws2812 programmable LEDs.
+ *  Author:       Chris Saiko
+ *  Email:        chris.saiko@gmail.com
+
+    This file is part of desk-led.
+
+    desk-led is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    desk-led is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with desk-led.  If not, see <https://www.gnu.org/licenses/>.
+    
+ */
+
+#include <FastLED.h>
+#define NUM_LEDS 6
+#define DATA_PIN 4
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
