a26b777befa1fae854d0bc758d1a12bf1e2b753f
Chris Saiko <chris.saiko@gmail.com>
2021-03-27 17:39:34 -0500

    initial argp options set

--- c/desk-led.c
+++ c/desk-led.c
@@ -22,8 +22,9 @@
 
 #include <stdlib.h>
 #include <argp.h>
+#include <string.h>
+
 // #include <fcntl.h>
-// #include <string.h>
 // #include <termios.h>
 // #include <unistd.h>
 // #include <errno.h>
@@ -39,18 +40,112 @@ const char *argp_program_bug_address =
 
 /* Program documentation. */
 static char doc[] =
-  "desk-led -- a program to control WS2812 LED strips attached to an Arduino";
+  "desk-led -- a program to control WS2812 LED strips attached to an Arduino.\n\n"
+  "            The arguments RED, GREEN, and BLUE are integers from 0 to 255,\n"
+  "            representing the component brightness of a color.";
 
 /* A description of the arguments we accept. */
-static char args_doc[] = "ARG1 ARG2";
+static char args_doc[] = "RED GREEN BLUE";
 
-/* Argument parser. */
-static struct argp argp = { 0, 0, 0, doc };
+/* The options we understand. */
+static struct argp_option options[] = {
+  {"verbose",  'v', 0,         0,  "Produce verbose output" },
+  {"quiet",    'q', 0,         0,  "Don't produce any output" },
+  {"silent",   's', 0,         OPTION_ALIAS },
+  {"gradient", 'g', "SECONDS", 0,  "Change color in a gradient over number of SECONDS" },
+  {"output",   'o', "FILE",    0,  "Output to FILE instead of standard output" },
+  {"config",   'c', "FILE",    0,  "Use the supplied FILE to send instructions" },
+  { 0 }
+};
+
+/* Used by main to communicate with parse_opt. */
+struct arguments
+{
+  char *args[2];                /* arg1 & arg2 */
+  int silent, verbose,gradient;
+  char *grad_seconds;
+  char *output_file;
+  char *config_file;
+};
+
+/* Parse a single option. */
+static error_t
+parse_opt (int key, char *arg, struct argp_state *state)
+{
+  /* Get the input argument from argp_parse, which we
+     know is a pointer to our arguments structure. */
+  struct arguments *arguments = state->input;
+
+  switch (key)
+    {
+    case 'q': case 's':
+      arguments->silent = 1;
+      break;
+    case 'v':
+      arguments->verbose = 1;
+      break;
+    case 'o':
+      arguments->output_file = arg;
+      break;
+    case 'c':
+      arguments->config_file = arg;
+      break;
+    case 'g':
+      arguments->gradient = 1;
+      arguments->grad_seconds = arg;
+      break;
+
+    case ARGP_KEY_ARG:
+      if (state->arg_num >= 3)
+        /* Too many arguments. */
+        argp_usage (state);
+
+      arguments->args[state->arg_num] = arg;
+
+      break;
+
+    case ARGP_KEY_END:
+      if (state->arg_num < 3)
+        /* Not enough arguments. */
+        argp_usage (state);
+      break;
+
+    default:
+      return ARGP_ERR_UNKNOWN;
+    }
+  return 0;
+}
+
+/* Our argp parser. */
+static struct argp argp = { options, parse_opt, args_doc, doc };
 
 
 int main(int argc, char** argv) {
 
-  argp_parse (&argp, argc, argv, 0, 0, 0);
+   struct arguments arguments;
+   
+   /* Default values. */
+   arguments.silent = 0;
+   arguments.verbose = 0;
+   arguments.gradient = 0;
+   arguments.grad_seconds = "0";
+   arguments.output_file = "-";
+   arguments.config_file = "-";
+
+   /* Parse our arguments; every option seen by parse_opt will be reflected in arguments. */
+   argp_parse (&argp, argc, argv, 0, 0, &arguments);
+
+   printf ("ARG1 = %s\nARG2 = %s\nARG3 = %s\n"
+           "OUTPUT_FILE = %s\nCONFIG_FILE = %s\nGRADIENT = %s\n"
+           "GRADIENT = %s seconds\nVERBOSE = %s\nSILENT = %s\n",
+           arguments.args[0], arguments.args[1], arguments.args[2],
+           arguments.output_file,
+           arguments.config_file,
+           arguments.gradient ? "yes" : "no",
+           arguments.grad_seconds,
+           arguments.verbose ? "yes" : "no",
+           arguments.silent ? "yes" : "no");
+
   exit (0);
 
 }
