a2648c8536c794c02fe6e52a7191e9a0639ac871
Chris Saiko <chris.saiko@gmail.com>
2021-03-26 19:51:56 -0500

    initial c code

---
+++ c/desk-led.h
@@ -0,0 +1,21 @@
+/*  File name:    desk-led.h
+ *  Description:  C code header file to control a number of LED strips using individual ws2812 programmable LEDs.
+ *  Author:       Chris Saiko
+ *  Email:        chris.saiko@gmail.com
+
+    This file is part of desk-led.
+
+    desk-led is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    desk-led is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with desk-led.  If not, see <https://www.gnu.org/licenses/>.
+    
+ */
